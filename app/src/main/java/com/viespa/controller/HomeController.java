28857f8035d0ce37c6695e7e215cd166c2ec8239
phuopham <108047552+phuopham@users.noreply.github.com>
2023-01-05 16:44:37 +0700

    add piechart and linechart

--- app/src/main/java/com/viespa/controller/HomeController.java
+++ app/src/main/java/com/viespa/controller/HomeController.java
@@ -4,11 +4,15 @@ import com.viespa.App;
 import com.viespa.models.User;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
@@ -27,8 +31,19 @@ public class HomeController implements Initializable {
     @FXML
     private NumberAxis yAxis;
 
+    @FXML
+    private PieChart pieChart;
 
-    void chart1(){
+    @FXML
+    private LineChart<Number, Number> lineChart;
+
+    @FXML
+    private CategoryAxis xAxisLC;
+
+    @FXML
+    private NumberAxis yAxisLC;
+
+    void setBarChart(){
         chart1.setTitle("Country Summary");
         xAxis.setLabel("Country");
         yAxis.setLabel("Value");
@@ -44,9 +59,48 @@ public class HomeController implements Initializable {
         chart1.getData().addAll(series1);
     }
 
+    void setPieChart(){
+        // Pie Chart
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Grapefruit", 13),
+                        new PieChart.Data("Oranges", 25),
+                        new PieChart.Data("Plums", 10),
+                        new PieChart.Data("Pears", 22),
+                        new PieChart.Data("Apples", 30));
+        pieChart.setData(pieChartData);
+        pieChart.setTitle("Imported Fruits");
+    }
+
+//    private void setLineChart() {
+//        xAxisLC.setLabel("Number of Month");
+//        yAxisLC.setLabel("Value");
+//        lineChart.setTitle("Stock Monitoring, 2010");
+//        //defining a series
+//        XYChart.Series series = new XYChart.Series();
+//        series.setName("My portfolio");
+//        //populating the series with data
+//        series.getData().add(new XYChart.Data(1, 23));
+//        series.getData().add(new XYChart.Data(2, 14));
+//        series.getData().add(new XYChart.Data(3, 15));
+//        series.getData().add(new XYChart.Data(4, 24));
+//        series.getData().add(new XYChart.Data(5, 34));
+//        series.getData().add(new XYChart.Data(6, 36));
+//        series.getData().add(new XYChart.Data(7, 22));
+//        series.getData().add(new XYChart.Data(8, 45));
+//        series.getData().add(new XYChart.Data(9, 43));
+//        series.getData().add(new XYChart.Data(10, 17));
+//        series.getData().add(new XYChart.Data(11, 29));
+//        series.getData().add(new XYChart.Data(12, 25));
+//
+//        lineChart.getData().add(series);
+//    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        chart1();
+        setBarChart();
+        setPieChart();
+//        setLineChart();
     }
 
 }
